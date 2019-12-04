f965d864a104b897ab21f17d451694db465d19d8
sormunen <sormunen.roni@gmail.com>
2019-12-04 11:21:49 +0200

    first commit, vuekello version 1 added

---
+++ kello/clo.js
@@ -0,0 +1,43 @@
+var clock = new Vue({
+    el: '#clock',
+    data: {
+        day: '',
+        time: '',
+        date: '',
+        weekNum:''
+    }
+});
+
+var week = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai'];
+var weekAmount = function(){
+    return new Date().getWeek();
+}
+// Palauttaa ISO-viikon päivämäärän.
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Tämän viikon torstai päättää vuoden.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+function updateTime() {
+    var cd = new Date();
+    clock.day = week[cd.getDay()-1];
+    clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock.date = zeroPadding(cd.getDate(), 2) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getFullYear(), 4);
+     clock.weekNum =  zeroPadding(weekAmount());
+};
+
+function zeroPadding(num, digit) {
+    var zero = '';
+    for(var i = 0; i < digit; i++) {
+        zero += '0';
+    }
+    return (zero + num).slice(-digit);
+}
