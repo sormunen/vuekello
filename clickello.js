ea41662f748e46a0c0d14bc13fc001bcfb8f76f3
sormunen <sormunen.roni@gmail.com>
2020-01-13 11:23:22 +0200

    bugfixes to pulse

--- clickello.js
+++ clickello.js
@@ -1,6 +1,11 @@
-new Vue ({
-    el: 'Vuekello',
+var show = new Vue ({
+    el: '#Vuekello',
     data: {
-        show: true
-    }
+        showT: true,
+        msg: 'viesti'
+    },
+    template: '<div class="pulse">{{msg}}</div>'
+
 })
+
+show.msg = 'Hello!'
