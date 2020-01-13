601bcaaaf513bc25909d30211c78e06b5cc84565
sormunen <sormunen.roni@gmail.com>
2020-01-13 13:52:25 +0200

    changing language of the clock in now an option, english and finnish available

--- clo.js
+++ clo.js
@@ -1,16 +1,41 @@
 var clock = new Vue({
     el: '#clock',
     data: {
-        today: 'Tänään on ',
+        today: 'Today is ',
         day: '',
         time: '',
         date: '',
-        wnText: 'Viikkonumero',
-        weekNum:''
+        wnText: 'Week number ',
+        weekNum:'',
+        button: {
+      text: 'Change Language'
+    },
+    lang: true
+    },
+    
+    methods: {
+      toggleLang: function() {
+        clock.lang = !clock.lang;
+        if(clock.button.text === 'Language') {
+          clock.today = clock.lang ? 'Today is ' : 'Tänään on ',
+          clock.wnText = clock.lang ? 'Week number ' : 'Viikkonumero ',
+          clock.button.text = clock.lang ? 'Chage Language' : ' Vaihda Kieli';
+        }
+        else
+        clock.today = clock.lang ? 'Tänään on ' :'Today is ',
+          clock.wnText = clock.lang ? 'Viikkonumero ' : 'Week number ',
+        clock.button.text = clock.lang ? 'Vaihda Kieli' : 'Change Language';
+        
+      }
     }
 });
 
-var week = ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'];
+var weekFi = ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'];
+var weekEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var weekDe = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+
+
+
 var weekAmount = function(){
     return new Date().getWeek();
 }
@@ -30,7 +55,13 @@ var timerID = setInterval(updateTime, 1000);
 updateTime();
 function updateTime() {
     var cd = new Date();
-    clock.day = week[cd.getDay()];
+    if(clock.button.text === 'Change Language') {
+      clock.day = weekEn[cd.getDay()];
+    }
+    else {
+      clock.day = weekFi[cd.getDay()];
+    }
+
     clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
     clock.date = zeroPadding(cd.getDate(), 2) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getFullYear(), 4);
      clock.weekNum =  zeroPadding(weekAmount());
@@ -43,3 +74,34 @@ function zeroPadding(num, digit) {
     }
     return (zero + num).slice(-digit);
 }
+
+/* var app = new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        { text: 'Sample Item 1' },
+        { text: 'Sample Item 2' },
+        { text: 'Sample Item 3' }
+      ],
+      button: {
+        text: 'Hide'
+      },
+      seen: true
+    },
+    methods: {
+      addItem: function() {
+        let item = document.getElementById("list-input").value;
+        let error = document.getElementById("error");
+        if (item == "") {
+          error.style.display = "block";
+        } else {
+          app.todos.push({ text: item });
+          error.style.display = "none";
+        }
+      },
+      toggleSeen: function() {
+        app.seen = !app.seen;
+        app.button.text = app.seen ? 'Hide' : 'Show';
+      }
+    }
+  }); */
